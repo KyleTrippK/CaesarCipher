9406323e58941dc8d983344ea53aaeaaf7121570
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-09-06 00:10:18 +0300

    Adds the encrypting class

--- src/main/java/Cipherdecrypt.java
+++ src/main/java/Cipherdecrypt.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
 public class Cipherdecrypt {
+
+
+    private String messages;
+    //    String messages = message.toUpperCase();
+    private int shiftKey;
+
+    public Cipherdecrypt(String messages, int shiftKey){
+        this.messages = messages;
+        this.shiftKey = shiftKey;
+    }
+
+    // Encrypt the message
+
+    public static String decrypt(String messages, int shiftKey){
+        String characters = "zyxwvutsrqponmlkjihgfedcba";
+        messages.toLowerCase();
+        StringBuffer cypherText = new StringBuffer();
+        for (int x=0; x<messages.length(); x++){
+            if(!Character.isLetter(messages.charAt(x))){
+                cypherText.append(messages.charAt(x));
+            }else{
+                int  character = characters.indexOf(messages.charAt(x));
+                int shift =(character + shiftKey)%26;
+                char cypher = characters.charAt(shift);
+                cypherText.append(cypher).toString();
+            }
+        }
+        return cypherText.toString();
+    }
+    public String getMessage(){
+
+        return messages;
+    }
+    public int getShiftKey(){
+
+        return shiftKey;
+    }
 }
+
+
