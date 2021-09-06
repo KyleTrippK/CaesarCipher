42941457e6d9c058256570300ca431a3573a4995
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-09-06 11:50:57 +0300

    Adds a test to test for encryption and decryption

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
+
+    //encryption tests
     @Test
     public void CaesarCipher_readsText_string() {
         CaesarCipher testCaesar = new CaesarCipher("message", 3);
@@ -15,6 +17,25 @@ public class CaesarCipherTest {
     }
 
     @Test
-    public void CaesarCipher_loops() {
+    public void CaesarCipher_encrypts_otd(){
+        CaesarCipher encrypt = new CaesarCipher("otd", 5);
+        assertEquals("otd", encrypt.encrypt("joy", 5));
+    }
+
+    // Decryption tests
+    @Test
+    public void CipherDecrypt_reads_input() {
+        CaesarCipher testCipher = new CaesarCipher("joy", 5);
+        assertEquals("joy", testCipher.getMessage());
+    }
+    @Test
+    public void CipherDecrypt_reads_shiftKey() {
+        CaesarCipher testCipher = new CaesarCipher("joy ", 5);
+        assertEquals(5, testCipher.getShiftKey());
+    }
+    @Test
+    public void CaesarDecrypt_decrypts_joy(){
+        CipherDecrypt decrypt = new CipherDecrypt("joy", 5);
+        assertEquals("joy", decrypt.decrypt("otd", 5));
     }
 }
